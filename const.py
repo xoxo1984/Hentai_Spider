3402a2720d2b6c5cd56418c2bfafb8a6b217f51f
xoxo1984 <xuewch@gmail.com>
2017-11-16 00:18:40 +0800

    fix a stuipid mistake

--- const.py
+++ const.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 CONFIG_FILE = 'config.ini' # 正式环境
-CONFIG_FILE = 'config_test.ini' # 测试环境
+# CONFIG_FILE = 'config_test.ini' # 测试环境
 GALLERY_INFO_FILE = 'info.txt'
 LAST_GALLERY_DOWNLOAD_TASK_FILE = 'last_gallery_download_task.json'
 FAIL_LOG_FILE = 'fail_log.txt'
